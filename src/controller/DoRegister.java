75d3765b7a91ec274a19af24e6bd7bc97fa39bf5
elephant28 <paminyoung@gmail.com>
2016-10-02 23:28:34 +0900

    Create New Project

---
+++ src/controller/DoRegister.java
@@ -0,0 +1,61 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Customer;
+import service.CustomerService;
+
+/**
+ * Servlet implementation class DoRegister
+ */
+@WebServlet("/doRegister")
+public class DoRegister extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public DoRegister() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		CustomerService service = CustomerService.getInstance();
+
+		String id = request.getParameter("id");
+		String password = request.getParameter("password");
+		String name = request.getParameter("name");
+		String gender = request.getParameter("gender");
+		String email = request.getParameter("email");
+
+		String page = null;
+
+		Customer newCustomer = new Customer(id, password, name, gender, email);
+		service.addCustomer(newCustomer);
+
+		if (id == null || password == null)
+			page = "/view/error.jsp";
+		else {
+			page = "/view/registerSuccess.jsp";
+			request.setAttribute("customer", newCustomer);
+		}
+
+		RequestDispatcher dispatcher = request.getRequestDispatcher(page);
+		dispatcher.forward(request, response);
+
+	}
+
+}
---
+++ src/service/CustomerService.java
@@ -0,0 +1,43 @@
+package service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import model.Customer;
+
+public class CustomerService {
+
+	private Map<String, Customer> customers;
+
+	private static final CustomerService instance = new CustomerService();
+
+	public CustomerService() {
+		customers = new HashMap<String, Customer>();
+	}
+
+	public static CustomerService getInstance() {
+		return instance;
+	}
+
+	public void addCustomer(Customer customer) {
+		customers.put(customer.getId(), customer);
+	}
+
+	public Customer findCustomer(String id) {
+		if (id != null)
+			return (customers.get(id.toLowerCase()));
+		else
+			return null;
+	}
+
+	public Customer login(String id, String password) {
+		Customer customerInfo = findCustomer(id);
+
+		if (customerInfo != null && customerInfo.getPassword().equals(password))
+			return customerInfo;
+		else
+			return null;
+
+	}
+
+}
